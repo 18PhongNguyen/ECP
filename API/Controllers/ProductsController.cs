2cf0e542ac727b25bacccaf61bcf9d310217a677
18PhongNguyen <147136144+18PhongNguyen@users.noreply.github.com>
2024-11-27 01:23:12 +0700

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,31 +11,49 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private readonly IProductRepository _repo;
 
-        private readonly StoreContext context;
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            this.context = context;
+            _repo = repo;
         }
 
         [HttpGet]
 
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-           var products = await context.Products.ToListAsync();
+           var products = await _repo.GetProductsAsync();
 
            return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProductA(int id)
         {
-            var product = await context.Products.FindAsync(id);
+            var product = await _repo.GetProductByIdAsync(id);
             if (product == null)
             {
                 return NotFound();
             }
             return Ok(product);
+        }
+
+        [HttpGet("types")]
+
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+           var productType = await _repo.GetProductTypesAsync();
+
+           return Ok(productType);
+        }
+
+        [HttpGet("brands")]
+
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+           var productBrand = await _repo.GetProductBrandsAsync();
+
+           return Ok(productBrand);
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,37 +1,55 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
-
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(GetConnectionString()));
-
-string GetConnectionString()
+internal class Program
 {
-    return builder.Configuration.GetConnectionString("DefaultConnection");
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(GetConnectionString()));
+        builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+        string GetConnectionString()
+        {
+            return builder.Configuration.GetConnectionString("DefaultConnection");
+        }
+
+        // Configure Kestrel
+        builder.WebHost.ConfigureKestrel(serverOptions =>
+        {
+            serverOptions.ListenAnyIP(5194); // HTTP
+            serverOptions.ListenAnyIP(5001, listenOptions => listenOptions.UseHttps()); // HTTPS
+        });
+
+        var app = builder.Build();
+
+        await using (var scope = app.Services.CreateAsyncScope())
+        {
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            try
+            {
+                var context = services.GetRequiredService<StoreContext>();
+                await context.Database.MigrateAsync();
+                await StoreContextSeed.SeedAsync(context,loggerFactory);
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "An error occured during migration");
+            }
+        }
+
+        // Configure the HTTP request pipeline.
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
 }
-
-// Configure Kestrel
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.ListenAnyIP(5194); // HTTP
-    serverOptions.ListenAnyIP(5001, listenOptions => listenOptions.UseHttps()); // HTTPS
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
-
